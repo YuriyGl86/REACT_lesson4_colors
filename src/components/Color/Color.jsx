c642b51bb5601d4a880f6538a6922d2dc4e61925
Yuriy Glushchenko <yuriygl86@gmail.com>
2023-10-27 18:12:18 +0300

    1

--- src/components/Color/Color.jsx
+++ src/components/Color/Color.jsx
@@ -11,27 +11,42 @@ export  function Color() {
         } : null;
       }
 
-    const initialState = '#00ffff'
+    const initialState = {
+        hex:'#00ffff',
+        rgb: '0 255 255'
+    }
     const [color, setColor] = useState(initialState)
     
-    const rgbRef = useRef()
+    const containerRef = useRef()
 
     const handleChange = ({ target }) => {
-        console.log(target)
+        setColor(prev => {
+            let newState = {...prev, [target.name]: target.value}
+            if(target.value.length < 7) return newState
+            const rgb = hexToRgb(target.value)
+            if(rgb){
+                newState = {...newState, 'rgb': `${rgb.r} ${rgb.g} ${rgb.b}`}
+                containerRef.current.style.background = target.value
+            } else {
+                newState = {...newState, 'rgb': `Ошибка!`}
+            }
+            return newState
+        })
+        
     }
 
     return (
-        <div className={classes.container}>
+        <div className={classes.container} ref ={ containerRef }>
             <form>
                 <input
                 name='hex' 
                 className={classes.input} 
                 type="text"
-                value = {color}
-                onСhange = {handleChange} 
+                value = {color.hex}
+                onChange = {handleChange} 
                 />
             </form>
-            <div className={classes.rgb} ref={rgbRef}></div>
+            <div className={classes.rgb}>{color.rgb}</div>
         </div>
     )
 }
